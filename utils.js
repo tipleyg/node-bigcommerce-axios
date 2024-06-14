8427edb772cbb4756ed0c388a48650396d4992de
tipleyg <44440231+tipleyg@users.noreply.github.com>
2024-06-14 17:31:50 -0500

    Update utils.js
    Added function for multi-columned CSVs

--- utils.js
+++ utils.js
@@ -20,6 +20,31 @@ export function dedupeArray(arr) {
     return Array.from(new Set(arr));
 }
 
+export function splitCsvRows(rows) {
+    let keys;
+    const csvData = [];
+
+    rows.forEach((row, i) => {
+        if (!i) {
+            //split header row to make keys
+            keys = row.split(',');            
+        } else {
+            const rowData = {},
+                values = row.split(',');
+
+            //form object from keys and row data
+            keys.forEach((key, i) => {
+                rowData[key.trim()] = values[i];
+            });
+
+            //push object to csvData;
+            csvData.push(rowData);
+        }
+    });
+
+    return csvData;
+}
+
 export function getParentSkuByVariantSku(sku) {
     try {
         if (!sku.includes("-")) throw "SKU in unexpected format";
