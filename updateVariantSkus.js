5576f4fb421d86b62eb1f7121071ad2ae2488aca
tipleyg <44440231+tipleyg@users.noreply.github.com>
2024-06-14 17:51:43 -0500

    Update updateVariantSkus.js
    adapted to modules approach

--- updateVariantSkus.js
+++ updateVariantSkus.js
@@ -1,125 +1,7 @@
-import axios from 'axios';
-import * as fs from 'fs';
+import AxiosBcConnection from './AxiosBcConnection.js';
+import { getArrayDataFromCSV, splitCsvRows, getParentSkuByVariantSku } from './utils.js';
 
-function getConfig() {
-    /*{
-        "logLevel": "info",
-        "clientId": "xxxx",
-        "secret": "xxxx",
-        "callback": "https://test.test/test",
-        "responseType": "json",
-        "storeHash": "storeHash",
-        "headers": { 
-            "Accept-Encoding": "*" 
-        },
-        "apiVersion": "v3"
-    }*/
-
-    try {
-        const configText = fs.readFileSync("config.json");
-        
-        if (!configText) return;
-        
-        return JSON.parse(configText);
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-function initAxiosHeaders(config) {
-    axios.defaults.headers.common['Accept'] = 'application/json';
-    axios.defaults.headers.common['Content-Type'] = 'application/json';
-    axios.defaults.headers.common['X-Auth-Token'] = config["x-auth-token"];
-}
-
-async function getArrayDataFromCSV(fileName) {
-    function splitCsvRows(rows) {
-        let keys;
-        const csvData = [];
-    
-        rows.forEach((row, i) => {
-            if (!i) {
-                //split header row to make keys
-                keys = row.split(',');            
-            } else {
-                const rowData = {},
-                    values = row.split(',');
-
-                //form object from keys and row data
-                keys.forEach((key, i) => {
-                    rowData[key.trim()] = values[i];
-                });
-
-                //push object to csvData;
-                csvData.push(rowData);
-            }
-        });
-
-        return csvData;
-    }
-
-    try {
-        const csvText = fs.readFileSync(fileName, 'utf8');
-        
-        if (!csvText) return;
-
-        return splitCsvRows(csvText.split("\n"));
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-async function updateProductVariants(config, variantData) {
-    const baseUrl = `https://api.bigcommerce.com/stores/${config.storeHash}/v3/catalog/products`;
-
-    const getParentSkuByVariantSku = sku => {
-        try {
-            if (!sku.includes("-")) throw "SKU in unexpected format";
-
-            return sku.split("-")[0];    //parent SKUs are known to be contained in variant SKUs before their first '-'
-        } catch(e) {
-            console.error(e);
-        }
-    };
-
-    async function getProductIdBySKU(sku) {
-        try {
-            const response = await axios.get(`${baseUrl}/?sku=${sku}&include_fields=`),
-                product = response.data.data[0];
-    
-            return product.id;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function getProductVariantByProdId(productId, sku) {
-        try {
-            const response = await axios.get(`${baseUrl}/${productId}/variants?sku=${sku}&include_fields=sku`),
-                variant = response.data.data[0];
-            
-            return variant.id;
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    async function updateProductVariant(productId, variant) {
-        const { variantId } = variant,
-            content = {
-                sku: variant.sku,
-                option_display_name: variant.productName
-            };
-
-        try {
-            const response = await axios.put(`${baseUrl}/${productId}/variants/${variantId}`, content);
-
-            console.log(`deleted ${productId}/${variantId} status:${response.status}`);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
+async function updateProductVariants({ getProductIdBySKU, getProductVariantByProdId, updateProductVariantName }, variantData) {
     for (const variant of variantData) {
         const variantSku = variant.sku,
             parentSku = getParentSkuByVariantSku(variantSku),
@@ -133,17 +15,18 @@ async function updateProductVariants(config, variantData) {
 
         variant.variantId = variantId;
 
-        await updateProductVariant(productId, variant);
+        await updateProductVariantName(productId, variant);
     }
 }
 
-(async () => {    
-    const variantData = await getArrayDataFromCSV("csv.csv");//"./hoistsDeleteVariantSkus.csv");
-    //console.log(JSON.stringify(variantData));
+async function updateVariantsBySku() {    
+    const aceCatalogBcConnection = new AxiosBcConnection(),
+        filename = process.argv[2] || "csv2.csv",
+        variantData = splitCsvRows(await getArrayDataFromCSV(filename));
     
-    const config = getConfig();
-    if (!config || !Object.keys(config).length) throw Error("no config");
-    initAxiosHeaders(config);
+    if (!variantData || !variantData.length) throw Error("import missing content!");
     
-    await updateProductVariants(config, variantData);
-})();
+    await updateProductVariants(aceCatalogBcConnection, variantData);
+}
+
+await updateVariantsBySku();
