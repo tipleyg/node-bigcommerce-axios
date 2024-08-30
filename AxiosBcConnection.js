c43ee5096a474d831806c7887d78b666f94b12c6
tipleyg <44440231+tipleyg@users.noreply.github.com>
2024-08-29 22:38:43 -0500

    Update AxiosBcConnection.js

--- AxiosBcConnection.js
+++ AxiosBcConnection.js
@@ -119,7 +119,7 @@ export default class AxiosBcConnection {
                 ERROR: TOO_MANY
                 ERROR: TOO_MANY
                 ERROR: TOO_MANY
-            `)
+            `);
             //BC indicated in BIGDEV training that too_many indicates that you received
             //less data than you requested in this page
             //todo: exclude the IDs that got included on this page and req the same-ish page with meta.pagination.current?
